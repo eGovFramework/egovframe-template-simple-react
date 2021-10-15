8625d979c6c04915915e1e431fbcce0778b29e5e
Jooho Yoon <platonicojju@gmail.com>
2021-10-15 17:50:23 +0900

    Feat:게시판 사용관리 등록/수정 구분

--- src/egov/admin/usage/EgovAdminUsageEdit.jsx
+++ src/egov/admin/usage/EgovAdminUsageEdit.jsx
@@ -142,47 +142,76 @@ function EgovAdminBoardEdit(props) {
                         <h2 className="tit_2">게시판사용 관리</h2>
 
                         <div className="board_view2">
-                            <dl>
-                                <dt><label htmlFor="">게시판명</label></dt>
-                                <dd>
-                                    {boardDetail.bbsNm}
-                                </dd>
-                            </dl>
-                            <dl>
-                                <dt><label htmlFor="">커뮤니티/동호회명</label></dt>
-                                <dd>
-                                    {boardDetail.cmmntyNm !== "" && boardDetail.cmmntyNm}
-                                </dd>
-                            </dl>
-                            <dl>
-                                <dt><label htmlFor="">사용여부</label><span className="req">필수</span></dt>
-                                <dd>
-                                    {/* <span className="f_rdo"><input type="radio" name="reptitSeCode" id="reptitSeCode1" title="사용" /><em>사용</em></span>
+                            {/* 등록일때 일때 */}
+                            {modeInfo.mode === CODE.MODE_CREATE && <>
+                                <dl>
+                                    <dt><label for="">게시판명</label><span className="req">필수</span></dt>
+                                    <dd>
+                                        <span className="f_search2 w_full">
+                                            <input type="text" name="" title="" id="" placeholder="" />
+                                            <button type="button">조회</button>
+                                        </span>
+                                    </dd>
+                                </dl>
+                                <dl>
+                                    <dt>커뮤니티/동호회명<span className="req">필수</span></dt>
+                                    <dd>
+                                        <label className="f_select " for="trgetType">
+                                            <select id="trgetType" name="trgetType" className="select" title=""
+                                                onChange={(e) => setBoardDetail({ ...boardDetail, trgetType: e.target.value, trgetId: "SYSTEM_DEFAULT_BOARD" })}>
+                                                <option value="">선택하세요</option>
+                                                <option value="SYSTEM">시스템</option>
+                                            </select>
+                                        </label>
+                                        <input className="f_input2" type="text" name="" title="" id="bbsNm" placeholder="" />
+                                    </dd>
+                                </dl>
+                            </>}
+
+                            {/* 수정/조회 일때 */}
+                            {modeInfo.mode === CODE.MODE_MODIFY && <>
+                                <dl>
+                                    <dt><label htmlFor="">게시판명</label></dt>
+                                    <dd>
+                                        {boardDetail.bbsNm}
+                                    </dd>
+                                </dl>
+                                <dl>
+                                    <dt><label htmlFor="">커뮤니티/동호회명</label></dt>
+                                    <dd>
+                                        {boardDetail.cmmntyNm !== "" && boardDetail.cmmntyNm}
+                                    </dd>
+                                </dl>
+                                <dl>
+                                    <dt><label htmlFor="">사용여부</label><span className="req">필수</span></dt>
+                                    <dd>
+                                        {/* <span className="f_rdo"><input type="radio" name="reptitSeCode" id="reptitSeCode1" title="사용" /><em>사용</em></span>
                                     <span className="f_rdo"><input type="radio" name="reptitSeCode" id="reptitSeCode2" title="미사용" /><em>미사용</em></span> */}
-                                    <EgovRadioButtonGroup
-                                        name="useAt"
-                                        radioGroup={useAtRadioGroup}
-                                        setValue={boardDetail.useAt}
-                                        setter={(v) => setBoardDetail({ ...boardDetail, useAt: v })} />
-                                </dd>
-                            </dl>
-                            <dl>
-                                <dt>제공 URL</dt>
-                                <dd>
-                                    <Link
-                                        to={{
-                                            pathname: boardDetail.provdUrl,
-                                            state: {
-                                                bbsId: boardDetail.bbsId,
+                                        <EgovRadioButtonGroup
+                                            name="useAt"
+                                            radioGroup={useAtRadioGroup}
+                                            setValue={boardDetail.useAt}
+                                            setter={(v) => setBoardDetail({ ...boardDetail, useAt: v })} />
+                                    </dd>
+                                </dl>
+                                <dl>
+                                    <dt>제공 URL</dt>
+                                    <dd>
+                                        <Link
+                                            to={{
+                                                // pathname: boardDetail.provdUrl,
+                                                pathname: URL.INFORM_NOTICE,
+                                                state: {
+                                                    bbsId: boardDetail.bbsId,
+                                                }
+                                            }}
+                                        >
+                                            {`${URL.INFORM_NOTICE}`}
+                                        </Link>
 
-                                            }
-                                        }}
-                                    >
-                                        {`${boardDetail.provdUrl}?bbsId=${boardDetail.bbsId}`}
-                                    </Link>
-
-                                </dd>
-                            </dl>
+                                    </dd>
+                                </dl>
+                            </>}
 
                             {/* <!-- 버튼영역 --> */}
                             <div className="board_btn_area">
