13c17fe4971880f4bc4ca8ab6c0de4816c94c5b7
hmmhmmhm <hmmhmmhm@naver.com>
2022-03-26 01:23:54 +0900

    feat: ESLint 와 Prettier 린트 추가

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: "all",
+  semi: true,
+  useTabs: false,
+  arrowParens: "always",
+  endOfLine: "auto",
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+};
