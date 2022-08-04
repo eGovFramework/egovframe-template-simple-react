bd4df512bf8cc70c648772bb9c61555a709b1216
yongfire38 <yongfire38@platformtech.co.kr>
2022-08-04 10:08:52 +0900

    Refactor: react 버전 업데이트 및 클라이언트 렌더링 API 수정

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
