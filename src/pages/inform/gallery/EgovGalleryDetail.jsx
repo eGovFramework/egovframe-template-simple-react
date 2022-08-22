17461cb27e88ee3f25d57c9f8b249f0c2d3b8b28
yongfire38 <yongfire38@platformtech.co.kr>
2022-08-22 14:28:24 +0900

    updated: context와 routes코드 레이어별 분리

---
+++ src/pages/inform/gallery/EgovGalleryDetail.jsx
@@ -0,0 +1,197 @@
+import React, { useState, useEffect } from 'react';
+
+import { Link, useHistory } from 'react-router-dom';
+
+import * as EgovNet from 'api/egovFetch';
+import URL from 'constants/url';
+import CODE from 'constants/code';
+import { GALLERY_BBS_ID } from 'config';
+
+import { default as EgovLeftNav } from 'components/leftmenu/EgovLeftNavInform';
+import EgovAttachFile from 'components/EgovAttachFile';
+import EgovImageGallery from 'components/EgovImageGallery';
+
+function EgovGalleryDetail(props) {
+    console.groupEnd("EgovGalleryDetail");
+    console.log("------------------------------");
+    console.log("EgovGalleryDetail [props] : ", props);
+
+    const history = useHistory();
+    console.log("EgovGalleryDetail [history] : ", history);
+
+    const bbsId = history.location.state.bbsId || GALLERY_BBS_ID;
+    const nttId = history.location.state.nttId;
+    const searchCondition = history.location.state.searchCondition;
+
+    const [masterBoard, setMasterBoard] = useState({});
+    const [user, setUser] = useState({});
+    const [boardDetail, setBoardDetail] = useState({});
+    const [boardAttachFiles, setBoardAttachFiles] = useState();
+
+    const retrieveDetail = () => {
+        const retrieveDetailURL = '/cop/bbs/selectBoardArticleAPI.do';
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                bbsId: bbsId,
+                nttId: nttId
+            })
+        }
+        EgovNet.requestFetch(retrieveDetailURL,
+            requestOptions,
+            function (resp) {
+                setMasterBoard(resp.result.brdMstrVO);
+                setBoardDetail(resp.result.boardVO);
+                setUser(resp.result.user);
+                setBoardAttachFiles(resp.result.resultFiles);
+            }
+        );
+    }
+
+    const onClickDeleteBoardArticle = (bbsId, nttId) => {
+        const deleteBoardURL = "/cop/bbs/deleteBoardArticleAPI.do";
+        const jToken = localStorage.getItem('jToken');
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': jToken
+            },
+            body: JSON.stringify({
+                bbsId: bbsId,
+                nttId: nttId
+            })
+        }
+
+        EgovNet.requestFetch(deleteBoardURL,
+            requestOptions,
+            (resp) => {
+                console.log("====>>> board delete= ", resp);
+                if (Number(resp.resultCode) === Number(CODE.RCV_SUCCESS)) {
+                    alert("게시글이 삭제되었습니다.")
+                    history.push(URL.INFORM_GALLERY);
+                } else {
+                    alert("ERR : " + resp.resultMessage);
+                }
+
+            }
+        );
+    }
+
+    useEffect(function () {
+        retrieveDetail();
+        return function () {
+        }
+    }, []);
+
+    console.groupEnd("EgovGalleryDetail");
+
+    return (
+        <div className="container">
+            <div className="c_wrap">
+                {/* <!-- Location --> */}
+                <div className="location">
+                    <ul>
+                        <li><Link to={URL.MAIN} className="home">Home</Link></li>
+                        <li><Link to={URL.INFORM}>알림마당</Link></li>
+                        <li>{masterBoard && masterBoard.bbsNm}</li>
+                        {/* brdMstrVO */}
+                    </ul>
+                </div>
+                {/* <!--// Location --> */}
+
+                <div className="layout">
+                    {/* <!-- Navigation --> */}
+                    <EgovLeftNav></EgovLeftNav>
+                    {/* <!--// Navigation --> */}
+
+                    <div className="contents NOTICE_VIEW" id="contents">
+                        {/* <!-- 본문 --> */}
+
+                        <div className="top_tit">
+                            <h1 className="tit_1">알림마당</h1>
+                        </div>
+
+                        <h2 className="tit_2">{masterBoard && masterBoard.bbsNm}</h2>
+
+                        {/* <!-- 게시판 상세보기 --> */}
+                        <div className="board_view">
+                            <div className="board_view_top">
+                                <div className="tit">{boardDetail && boardDetail.nttSj}</div>
+                                <div className="info">
+                                    <dl>
+                                        <dt>작성자</dt>
+                                        <dd>{boardDetail && boardDetail.frstRegisterNm}</dd>
+                                    </dl>
+                                    <dl>
+                                        <dt>작성일</dt>
+                                        <dd>{boardDetail && boardDetail.frstRegisterPnttm}</dd>
+                                    </dl>
+                                    <dl>
+                                        <dt>조회수</dt>
+                                        <dd>{boardDetail && boardDetail.inqireCo}</dd>
+                                    </dl>
+                                </div>
+                            </div>
+
+                            <div className="board_article">
+                                <textarea name="" cols="30" rows="10" readOnly="readonly" defaultValue={boardDetail && boardDetail.nttCn}></textarea>
+                            </div>
+
+                            <EgovImageGallery boardFiles={boardAttachFiles} />
+
+                            <div className="board_attach">
+                                <EgovAttachFile boardFiles={boardAttachFiles} />
+                            </div>
+
+
+                            <div className="board_btn_area">
+                                {user.id &&
+                                    <div className="left_col btn3">
+                                        <Link to={{
+                                            pathname: URL.INFORM_GALLERY_MODIFY,
+                                            state: {
+                                                nttId: nttId,
+                                                bbsId: bbsId
+                                            }
+                                        }} className="btn btn_skyblue_h46 w_100">수정</Link>
+                                        <a href="" className="btn btn_skyblue_h46 w_100" onClick={(e) => {
+                                            e.preventDefault();
+                                            onClickDeleteBoardArticle(boardDetail.bbsId, boardDetail.nttId);
+                                        }}>삭제</a>
+                                        <Link to={{
+                                            pathname: URL.INFORM_GALLERY_REPLY,
+                                            state: {
+                                                nttId: nttId,
+                                                bbsId: bbsId
+                                            }
+                                        }} className="btn btn_skyblue_h46 w_100">답글작성</Link>
+                                    </div>
+                                }
+                                <div className="right_col btn1">
+                                    <Link to={{
+                                        pathname: URL.INFORM_GALLERY,
+                                        state: {
+                                            nttId: nttId,
+                                            bbsId: bbsId,
+                                            searchCondition: searchCondition
+                                        }
+                                    }} className="btn btn_blue_h46 w_100">목록</Link>
+                                </div>
+                            </div>
+                        </div>
+                        {/* <!-- 게시판 상세보기 --> */}
+
+                        {/* <!--// 본문 --> */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default EgovGalleryDetail;
---
+++ src/pages/inform/notice/EgovNoticeList.jsx
@@ -0,0 +1,208 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
+import * as EgovNet from 'api/egovFetch';
+import URL from 'constants/url';
+import { NOTICE_BBS_ID } from 'config';
+
+import { default as EgovLeftNav } from 'components/leftmenu/EgovLeftNavInform';
+import EgovPaging from 'components/EgovPaging';
+
+import { itemIdxByPage } from 'utils/calc';
+
+function EgovNoticeList(props) {
+    console.group("EgovNoticeList");
+    console.log("[Start] EgovNoticeList ------------------------------");
+    console.log("EgovNoticeList [props] : ", props);
+
+    const history = useHistory();
+    console.log("EgovNoticeList [history] : ", history);
+
+    const bbsId = history.location.state?.bbsId || NOTICE_BBS_ID; 
+    let searchCnd = '0';
+    let searchWrd = '';
+
+    const [searchCondition, setSearchCondition] = useState(history.location.state?.searchCondition || { bbsId: bbsId, pageIndex: 1, searchCnd: '0', searchWrd: '' });// 기존 조회에서 접근 했을 시 || 신규로 접근 했을 시
+    const [masterBoard, setMasterBoard] = useState({});
+    const [user, setUser] = useState({});
+    const [paginationInfo, setPaginationInfo] = useState({});
+
+    const [listTag, setListTag] = useState([]);
+
+    const retrieveList = (searchCondition) => {
+        console.groupCollapsed("EgovNoticeList.retrieveList()");
+
+        const retrieveListURL = '/cop/bbs/selectBoardListAPI.do';
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(searchCondition)
+        }
+
+        EgovNet.requestFetch(retrieveListURL,
+            requestOptions,
+            (resp) => {
+                setMasterBoard(resp.result.brdMstrVO);
+                setPaginationInfo(resp.result.paginationInfo);
+                setUser(resp.result.user);
+
+                let mutListTag = [];
+                listTag.push(<p className="no_data">검색된 결과가 없습니다.</p>); // 게시판 목록 초기값
+                
+                const resultCnt = parseInt(resp.result.resultCnt);
+                const currentPageNo = resp.result.paginationInfo.currentPageNo;
+                const pageSize = resp.result.paginationInfo.pageSize;
+
+                // 리스트 항목 구성
+                resp.result.resultList.forEach(function (item, index) {
+                    if (index === 0) mutListTag = []; // 목록 초기화
+                    const listIdx = itemIdxByPage(resultCnt , currentPageNo, pageSize, index);
+
+                    mutListTag.push(
+                        <Link
+                            to={{
+                                pathname: URL.INFORM_NOTICE_DETAIL,
+                                state: {
+                                    nttId: item.nttId,
+                                    bbsId: item.bbsId,
+                                    searchCondition: searchCondition
+                                }
+                            }}
+                            key={listIdx}
+                            className="list_item" >
+                            <div>{listIdx}</div>
+                            {(item.replyLc * 1 ? true : false) &&
+                                <div className="al reply">
+                                    {item.nttSj}
+                                </div>}
+                            {(item.replyLc * 1 ? false : true) &&
+                                <div className="al">
+                                    {item.nttSj}
+                                </div>}
+                            <div>{item.frstRegisterNm}</div>
+                            <div>{item.frstRegisterPnttm}</div>
+                            <div>{item.inqireCo}</div>
+                        </Link>
+                    );
+                });
+                setListTag(mutListTag);
+            },
+            function (resp) {
+                console.log("err response : ", resp);
+            }
+        );
+        console.groupEnd("EgovNoticeList.retrieveList()");
+    }
+
+    useEffect(() => {
+        retrieveList(searchCondition);
+        return () => {
+        }
+    }, [searchCondition]);
+
+    console.log("------------------------------EgovNoticeList [End]");
+    console.groupEnd("EgovNoticeList");
+    return (
+        <div className="container">
+            <div className="c_wrap">
+                {/* <!-- Location --> */}
+                <div className="location">
+                    <ul>
+                        <li><Link to={URL.MAIN} className="home">Home</Link></li>
+                        <li><Link to={URL.INFORM}>알림마당</Link></li>
+                        {/* <li>공지사항</li> */}
+                        <li>{masterBoard && masterBoard.bbsNm}</li>
+                    </ul>
+                </div>
+                {/* <!--// Location --> */}
+
+                <div className="layout">
+                    {/* <!-- Navigation --> */}
+                    <EgovLeftNav></EgovLeftNav>
+                    {/* <!--// Navigation --> */}
+
+                    <div className="contents NOTICE_LIST" id="contents">
+                        {/* <!-- 본문 --> */}
+
+                        <div className="top_tit">
+                            <h1 className="tit_1">알림마당</h1>
+                        </div>
+
+                        {/* <h2 className="tit_2">공지사항</h2> */}
+                        <h2 className="tit_2">{masterBoard && masterBoard.bbsNm}</h2>
+
+                        {/* <!-- 검색조건 --> */}
+                        <div className="condition">
+                            <ul>
+                                <li className="third_1 L">
+                                    <label className="f_select" htmlFor="sel1">
+                                        <select id="sel1" title="조건" defaultValue={searchCondition.searchCnd}
+                                            onChange={e => {
+                                                searchCnd = e.target.value;
+                                                // setSearchCondition({ ...searchCondition, searchCnd: e.target.value });
+                                            }}
+                                        >
+                                            <option value="0">제목</option>
+                                            <option value="1">내용</option>
+                                            <option value="2">작성자</option>
+                                        </select>
+                                    </label>
+                                </li>
+                                <li className="third_2 R">
+                                    <span className="f_search w_500">
+                                        <input type="text" name="" defaultValue={searchCondition.searchWrd} placeholder=""
+                                            onChange={e => {
+                                                searchWrd = e.target.value;
+                                                //setSearchCondition({ ...searchCondition, searchWrd: e.target.value });
+                                            }}
+                                        />
+                                        <button type="button"
+                                            onClick={e => {
+                                                setSearchCondition({ ...searchCondition, pageIndex: 1, searchCnd: searchCnd, searchWrd: searchWrd });
+                                            }}>조회</button>
+                                    </span>
+                                </li>
+                                {user.id &&
+                                    <li>
+                                        <Link to={URL.INFORM_NOTICE_CREATE} className="btn btn_blue_h46 pd35">등록</Link>
+                                    </li>
+                                }
+                            </ul>
+                        </div>
+                        {/* <!--// 검색조건 --> */}
+
+                        {/* <!-- 게시판목록 --> */}
+                        <div className="board_list BRD002">
+                            <div className="head">
+                                <span>번호</span>
+                                <span>제목</span>
+                                <span>작성자</span>
+                                <span>작성일</span>
+                                <span>조회수</span>
+                            </div>
+                            <div className="result">
+                                {listTag}
+                            </div>
+                        </div>
+                        {/* <!--// 게시판목록 --> */}
+
+                        <div className="board_bot">
+                            {/* <!-- Paging --> */}
+                            <EgovPaging pagination={paginationInfo} moveToPage={passedPage => {
+                                setSearchCondition({ ...searchCondition, pageIndex: passedPage });
+                            }}></EgovPaging>
+                            {/* <!--/ Paging --> */}
+                        </div>
+
+                        {/* <!--// 본문 --> */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default EgovNoticeList;
