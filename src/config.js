5accbbfced85bfe330f5f79682f37e1e5b7a2db2
eGovFrameSupport <egovframesupport@gmail.com>
2025-03-07 10:40:39 +0900

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // Configuration for different environments
-export const SERVER_URL = process.env.NODE_ENV === 'test' ? 'http://localhost:8080' : undefined;
+export const SERVER_URL = process.env.NODE_ENV === 'test' ? 'http://localhost:8080' : 'http://localhost:8080';
 
 export const DEFAULT_BBS_ID = "BBSMSTR_AAAAAAAAAAAA"; // default = 공지사항 게시판 아이디
 export const NOTICE_BBS_ID = "BBSMSTR_AAAAAAAAAAAA"; // 공지사항 게시판 아이디
