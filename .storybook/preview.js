7ccf70e6c88b1787dc3e0f0fa1d1ae193ba798d9
MatchaKim <kimjunsik333@gmail.com>
2025-08-27 21:29:04 +0900

    setting : storybook css 적용

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,16 @@
 /** @type { import('@storybook/react-webpack5').Preview } */
+import "../src/css/base.css";
+import "../src/css/layout.css";
+import "../src/css/component.css";
+import "../src/css/page.css";
+import "../src/css/response.css";
+
 const preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
