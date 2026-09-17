b65ad4f0856b0ffb2cc96aa4f833a1fe02c356da
ch <fla0242@nate.com>
2026-09-17 20:54:38 +0900

    chore: detect React version in ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,7 @@ module.exports = {
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: { react: { version: "18.2" } },
+  settings: { react: { version: "detect" } },
   plugins: ["react-refresh"],
   rules: {
     "react/jsx-no-target-blank": "error",
